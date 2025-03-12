40c12e4226c9c9325100df4732cb820dbae13c65
micheledidonna01 <micheledidonna337@gmail.com>
2025-03-12 15:07:18 +0100

    feat: snack 8

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,9 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+let classe = students.find(student => {
+  return student.name === 'Marco Lanci';
+});
+
+console.log(classe.class);
