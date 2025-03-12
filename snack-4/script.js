aec100571599eb76c181601c80e015a61d971593
micheledidonna01 <micheledidonna337@gmail.com>
2025-03-12 14:47:45 +0100

    feat: snack 4

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,9 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+let newArray = posts.map(post =>{
+  return post.author;
+});
+
+console.log(newArray);
